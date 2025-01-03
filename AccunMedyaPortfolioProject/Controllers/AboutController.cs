41229d948006d05890b892da44ef51e382d62e15
emine nur <Lenovo@DESKTOP-50D6PVA>
2025-01-03 18:32:22 +0300

    About,Project için controller ve viewler oluşturdum ve create update delete ve list işlemlerini bitirdim

---
+++ AccunMedyaPortfolioProject/Controllers/AboutController.cs
@@ -0,0 +1,62 @@
+﻿using AccunMedyaPortfolioProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AccunMedyaPortfolioProject.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: About
+        PortfolioDBEntities1 db = new PortfolioDBEntities1();
+        public ActionResult Index()
+        {
+            var aboutList = db.Abouts.ToList();
+            return View(aboutList);
+        }
+        [HttpGet]
+        public ActionResult CreateAbout()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateAbout(About about)
+        {
+            db.Abouts.Add(about);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DeleteAbout(int id)
+        {
+            var value = db.Abouts.Find(id);
+            db.Abouts.Remove(value);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+        [HttpGet]
+
+        public ActionResult UpdateAbout(int id)
+        {
+            var about = db.Abouts.Find(id);
+            return View(about);
+        }
+
+        [HttpPost]
+
+        public ActionResult UpdateAbout(About about)
+        {
+            var updatedAbout = db.Abouts.Find(about.Id);
+            updatedAbout.Title = about.Title;
+            updatedAbout.Description = about.Description;
+            updatedAbout.CVLink = about.CVLink;
+            updatedAbout.ImageUrl = about.ImageUrl;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ AccunMedyaPortfolioProject/Controllers/ProjectController.cs
@@ -0,0 +1,69 @@
+﻿using AccunMedyaPortfolioProject.Models;
+using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AccunMedyaPortfolioProject.Controllers
+{
+    public class ProjectController : Controller
+    {
+        // GET: Project
+        PortfolioDBEntities1 db = new PortfolioDBEntities1();
+        public ActionResult Index()
+        {
+            var projects = db.Projects.ToList();
+            return View(projects);
+        }
+        [HttpGet]
+        public ActionResult CreateProject()
+        {
+            var categories = db.Categories.ToList();
+            var list = new SelectList(categories, "Id", "Name");
+            ViewBag.Categories = list;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateProject(Project project)
+        {
+            db.Projects.Add(project);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DeleteProject(int id)
+        {
+            var value = db.Projects.Find(id);
+            db.Projects.Remove(value);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+
+        public ActionResult UpdateProject(int id)
+        {
+            var project = db.Projects.Find(id);
+            var categories = db.Categories.ToList();
+            var list = new SelectList(categories, "Id", "Name", project.CategoryId);
+            ViewBag.Categories = list;
+            return View(project);
+
+        }
+
+        [HttpPost]
+
+        public ActionResult UpdateProject(Project project)
+        {
+            var updatedProject = db.Projects.Find(project.Id);
+            updatedProject.ProjectName=project.ProjectName;
+            updatedProject.CategoryId=project.CategoryId;
+            updatedProject.CoverImageUrl=project.CoverImageUrl;
+            updatedProject.Description=project.Description;
+            updatedProject.ProjectLink=project.ProjectLink;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
